a55f2aa0fa04fb5b16b18e30501ef434dcb6d9d9
Ahmad Hassan <159900133+Ahmadhassan011@users.noreply.github.com>
2024-12-20 02:50:13 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int numbers[5] = {1, 2, 3, 4, 5};
+    int numbers[5] = {1, 2, 3, 4, 5, 6};
     
     cout << "Array elements: ";
     for (int i = 0; i < 5; ++i) {
